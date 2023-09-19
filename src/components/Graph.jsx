68a42abb9269612ba90bf31628e18a324167535a
Hosh <domingohoshaiah@gmail.com>
2023-09-19 16:01:38 +0800

    put graph mapping in a function

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -25,41 +25,44 @@ const Graph = () => {
 
 
     useEffect(() => {
-        const shortestPath = new Set(graphState.shortestPath)
-        const selectedNodes = new Set(viewState.selectedNodes)
-        const nodeOccupancy = graphState.nodeOccupancy
-        const adjacencyList = graphState.adjacencyList
-        const gridSize = Math.sqrt(Object.keys(adjacencyList).length);
-        const rowToSet = Array.from({ length: gridSize }, (_, rowIndex) => {
-            const nodesInRow = Object.keys(adjacencyList).filter(node => {
-                const [row] = node.split('-');
-                return parseInt(row, 10) === rowIndex;
+        const mapNodes = () => {
+            const shortestPath = new Set(graphState.shortestPath)
+            const selectedNodes = new Set(viewState.selectedNodes)
+            const nodeOccupancy = graphState.nodeOccupancy
+            const adjacencyList = graphState.adjacencyList
+            const gridSize = Math.sqrt(Object.keys(adjacencyList).length);
+            const rowToSet = Array.from({ length: gridSize }, (_, rowIndex) => {
+                const nodesInRow = Object.keys(adjacencyList).filter(node => {
+                    const [row] = node.split('-');
+                    return parseInt(row, 10) === rowIndex;
+                });
+        
+                return (
+                    <div key={rowIndex} className="flex flex-col-reverse">
+                        {nodesInRow.map(node => {
+                            const [row, col] = node.split('-');
+                            const nodeInShortestPath = shortestPath.has(node)
+                            const nodeIsSelected = selectedNodes.has(node)
+                            const nodeIsOccupied = nodeOccupancy[node] === 'occupied'
+                            return (
+                                <button onClick={() => {handleNodeClick(node, nodeIsSelected)}} key={node} className={`border-black border w-8 h-8 flex items-center justify-center 
+                                ${nodeInShortestPath && viewState.currentView === 'shortestPath' ? 'bg-yellow-300' : ''}
+                                ${nodeIsOccupied ? 'bg-black': ''}
+                                ${viewState.currentView === "selectView" && nodeIsSelected ? 'border-2 border-yellow-400': ''}
+                                `} >
+                                    {node}
+                                </button>
+                            );
+                        })}
+                    </div>
+                );
             });
-    
-            return (
-                <div key={rowIndex} className="flex flex-col-reverse">
-                    {nodesInRow.map(node => {
-                        const [row, col] = node.split('-');
-                        const nodeInShortestPath = shortestPath.has(node)
-                        const nodeIsSelected = selectedNodes.has(node)
-                        const nodeIsOccupied = nodeOccupancy[node] === 'occupied'
-                        return (
-                            <button onClick={() => {handleNodeClick(node, nodeIsSelected)}} key={node} className={`border-black border w-8 h-8 flex items-center justify-center 
-                            ${nodeInShortestPath && viewState.currentView === 'shortestPath' ? 'bg-yellow-300' : ''}
-                            ${nodeIsOccupied ? 'bg-black': ''}
-                            ${viewState.currentView === "selectView" && nodeIsSelected ? 'border-2 border-yellow-400': ''}
-                            `} >
-                                {node}
-                            </button>
-                        );
-                    })}
-                </div>
-            );
-        });
-        setRows(rowToSet)
-        // console.log(deepCompare(graphState.adjacencyList, graphState.originalAdjacencyList))
-        // console.log(graphState.originalAdjacencyList)
-        // console.log(graphState.adjacencyList)
+            setRows(rowToSet)
+            // console.log(deepCompare(graphState.adjacencyList, graphState.originalAdjacencyList))
+            // console.log(graphState.originalAdjacencyList)
+            // console.log(graphState.adjacencyList)
+        }
+        mapNodes()
     }, [graphState.shortestPath, viewState.selectedNodes, graphState.nodeOccupancy, graphState.adjacencyList, viewState.currentView])
 
 
