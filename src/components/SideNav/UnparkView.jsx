a70809c909a15acff74d6a2acce8efb122211c8f
Hosh <domingohoshaiah@gmail.com>
2023-09-21 16:42:23 +0800

    added uparking cost

--- src/components/SideNav/UnparkView.jsx
+++ src/components/SideNav/UnparkView.jsx
@@ -1,22 +1,87 @@
 import { useDispatch, useSelector } from "react-redux";
-import { dijkstra, findShortestPath } from "../../utils/graphUtils";
+import { calculateHourDifference, calculateParkingCost, dijkstra, findShortestPath, getDateTimeNow, getParkedCarNodes, validateDateFormat } from "../../utils/graphUtils";
 import {  setShortestPath } from "../../redux/graphSlice";
 import { setCurrentView } from "../../redux/viewSlice";
+import { useState, useEffect } from "react";
 
 const UnparkView = () => {
     const dispatch = useDispatch()
     const graph = useSelector(state => state.graph)
+    const [parkedCarsSelection, setParkedCarsSelection] = useState([])
+    const [parkedCarInput, setParkedCarInput] = useState(getParkedCarNodes(graph.nodeOccupancy)[0])
+    const [dateTimeHelperText, setDateTimeHelperText] = useState('')
+    const [dateInput, setDateInput]= useState(getDateTimeNow())
+    const [receipt, setReceipt] = useState({})
 
-    const handleFindShortestPath = () => {
-        const data = dijkstra(graph.adjacencyList, "0-0")
-        const shortestPath = findShortestPath(data.distances, data.previous, "0-0","14-14")
-        dispatch(setShortestPath(shortestPath))
-        dispatch(setCurrentView('parkView'))
+    const handleUnparkCar = () => {
+        const isDateValid = validateDateFormat(dateInput)
+        if(!isDateValid) {
+            setDateTimeHelperText('Date format is invalid')
+            return;
+        }
+        
+        const numberOfHoursParked = calculateHourDifference(graph.nodeOccupancy[parkedCarInput].entryTime, dateInput)
+        console.log(graph.nodeOccupancy[parkedCarInput].entryTime)
+        console.log(dateInput)
+        console.log(numberOfHoursParked)
+        console.log(calculateParkingCost(numberOfHoursParked, graph.nodeOccupancy[parkedCarInput].parking))
+        setReceipt(calculateParkingCost(numberOfHoursParked, graph.nodeOccupancy[parkedCarInput].parking))
     }
+
+    const handleDateInputChange = (e) => {
+        setDateTimeHelperText('')
+        const inputValue = e.target.value;
+        const formattedDate = inputValue.replace('T', ' ');
+        setDateInput(formattedDate);
+    }
+
+    useEffect(() => {
+        const getParkedCarsNodesList= () => {
+            const parkedCarNodes= getParkedCarNodes(graph.nodeOccupancy)
+            setParkedCarsSelection(parkedCarNodes)
+        }
+        getParkedCarsNodesList()
+    }, [graph.nodeOccupancy])
 
     return (
         <div>
-            <button className="bg-slate-400 text-black p-1 rounded-sm" onClick={handleFindShortestPath}>Find Shortest Path</button>
+            <div className="flex">
+                <h1>Parked Vehicles:</h1>
+                <select
+                    value={parkedCarInput}
+                    onChange={e => setParkedCarInput(e.target.value)}
+                    className="border p-2 rounded"
+                >
+                    {parkedCarsSelection.map((item, index) => (
+                        <option key={index} value={item}>
+                            {item}
+                        </option>
+                    ))}
+                </select>
+            </div>
+            <div className="flex">
+                <h1>Date and time of exit:</h1>
+                <p>{dateTimeHelperText}</p>
+                <input
+                    type="datetime-local"
+                    value={dateInput}
+                    onChange={(e) => handleDateInputChange(e)}
+                    step="60" // Set step to 60 seconds (1 minute)
+                />
+            </div>
+            <div className="flex">
+                <h1>Bill Breakdown</h1>
+                <div>
+                </div>
+            </div>
+            <button className="bg-slate-400 text-black p-1 rounded-sm" onClick={handleUnparkCar}>Unpark</button>
+            <div className="flex flex-col">
+                <h1>{`Number of hours parked: ~ ${Math.ceil(receipt.totalHours)}hrs (${receipt.totalHours}hrs)`}</h1>
+                {receipt.flatHourTotal && <h1>{`First 3 hours: ${receipt.flatHourTotal}`}</h1>}
+                {receipt.fullDayCosts && <h1>{`Number of full days(${receipt.numberOfFullDays}) x 5000: ${receipt.fullDayCosts} `}</h1>}
+                {receipt.continuousTotal && <h1>{`${receipt.continuousHours}hr x ${receipt.parkingSize} parking: ${receipt.continuousTotal} `}</h1>}
+                <h1>{`Total: Php ${receipt.total}`}</h1>
+            </div>
         </div>
     )
 }
--- src/utils/graphUtils.js
+++ src/utils/graphUtils.js
@@ -247,6 +247,84 @@ export const getEntranceNodes = (occupancy) => {
     return filteredNodes
 }
 
+export const getParkedCarNodes= (occupancy) => {
+    const filteredNodes = Object.entries(occupancy)
+        .filter(([key, value]) => ['small','medium','large'].includes(value.parkedCar))
+        .map(([key, value]) => key);
+
+    return filteredNodes
+}
+
+export const calculateHourDifference = (dateString1, dateString2) => {
+    const date1 = new Date(dateString1);
+    const date2 = new Date(dateString2);
+
+    if (date2 < date1) {
+       return -1 
+    }
+
+    const differenceInMilliseconds = Math.abs(date2 - date1);
+    const differenceInHours = differenceInMilliseconds / (1000 * 60 * 60);
+
+    return differenceInHours;
+}
+
+export const calculateParkingCost = (hours, parkingSize) => {
+    const parkingPrices = {
+        small: 20,
+        medium: 60,
+        large: 100
+    }
+    let costBreakdown = {}
+    let runningHour = hours
+    if(runningHour <= 3) {
+         costBreakdown = {
+             flatHours: Math.ceil(runningHour),
+             flatHourTotal: 40,
+
+             total: 40,
+             totalHours: Math.round(hours*100)/100
+         }
+         return costBreakdown
+    } 
+    if(hours <= 24) {
+        runningHour -= 3
+        const continuousTotal = Math.ceil(runningHour) * parkingPrices[parkingSize]
+        costBreakdown = {
+            flatHours: 3,
+            flatHourTotal: 40,
+
+            continuousHours: Math.ceil(runningHour),
+            continuousTotal: continuousTotal,
+
+            parkingSize: parkingSize,
+            total: 40 + continuousTotal,
+            totalHours: Math.round(hours*100)/100
+        }
+        return costBreakdown
+    }
+
+    if(hours> 24) {
+        const numberOfFullDaysParked = Math.floor(runningHour/24)
+        const fullDayCosts = numberOfFullDaysParked * 5000
+        runningHour -= numberOfFullDaysParked * 24
+        
+        const continuousTotal = Math.ceil(runningHour) * parkingPrices[parkingSize]
+        costBreakdown = {
+            numberOfFullDays: numberOfFullDaysParked,
+            fullDayCosts: fullDayCosts,
+
+            continuousHours: Math.ceil(runningHour),
+            continuousTotal: continuousTotal,
+
+            parkingSize: parkingSize,
+            total: fullDayCosts + continuousTotal,
+            totalHours: Math.round(hours*100)/100
+        }
+        return costBreakdown
+    }
+}
+
 export const getDateTimeNow = () => {
     const now = new Date();
     const year = now.getFullYear().toString().padStart(4, '0');
