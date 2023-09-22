8ef5052ec2ca6a01e9492c65a7bb41fe97f546aa
Hosh <domingohoshaiah@gmail.com>
2023-09-22 21:33:38 +0800

    added notes

--- src/components/SideNav/EditView.jsx
+++ src/components/SideNav/EditView.jsx
@@ -100,13 +100,20 @@ const EditView = () => {
                 ))}
             </div>
             <div className="flex flex-col w-full ml-2 mt-2 text-neutral-100 font-semibold text-md"> {'Edit nodes'}</div>
-            <div className="flex flex-col w-full h-full items-center bg-neutral-200">
+            <div className="flex flex-col w-full h-full items-center bg-neutral-200 overflow-y-scroll overflow-x-hidden"> 
                 <button className="bg-neutral-50 text-black font-semibold p-1 rounded-md w-40 h-12 mt-4 mb-2" onClick={handleResetClick} >Reset</button>
                 <button className="bg-saffron text-white font-semibold p-1 rounded-md w-40 h-12 my-2" onClick={() => handleParkingClick('small')}>Small Parking</button>
                 <button className="bg-sandy text-white font-semibold p-1 rounded-md w-40 h-12 my-2" onClick={() => handleParkingClick('medium')}>Medium Parking</button>
                 <button className="bg-pink text-white font-semibold p-1 rounded-md w-40 h-12 my-2" onClick={() => handleParkingClick('large')}>Large Parking</button>
                 <button className={`${parkingDisabled ? 'bg-gray-400': 'bg-tree'} text-white font-semibold p-1 rounded-md w-40 h-12 my-2`} onClick={handleBlockClick} disabled={parkingDisabled}>Block</button>
                 <button className={`${entranceDisabled ? 'bg-gray-400': 'bg-teal'} text-white font-semibold p-1 rounded-md w-40 h-12 mb-4 mt-2`} onClick={() => handleEntranceClick()} disabled={entranceDisabled}>Entrance</button>
+                <div className="p-3 text-sm text-gray-800">
+                    <h1>Notes:</h1>
+                    <ul className="list-disc pl-5">
+                        <li>There must be at least three entrance blocks present to perform other operations.</li>
+                        <li>Entrances may only be put on edges</li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
